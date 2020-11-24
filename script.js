6e0628077fe9882469d172b324c8bdb87b9160a6
Soumyo78 <soumyo78@yahoo.com>
2020-11-24 17:35:26 +0530

    Search option added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let card_pos = [];
+
+
 let faqs = [
     {question: "1. What is the capital of India ?", answer: "Ans: New Delhi."},
     {question: "2. How many states are there in India ?", answer: "Ans: 29 states and 7 union territories."},
@@ -9,11 +12,12 @@ let faqs = [
     {question: "8. What is the National Anthem of India ?", answer: "Ans: 'Jan gana mana' written by Rabindranath Tagore."},
     {question: "9. What is the area of India ?", answer: "Ans: Over 3.3 million square kilometers."},
     {question: "10. What is the Length of India's Coastline ?", answer: "Ans: India's coastline extends for 7600 km."},
-    {question: "11. Who is teh current President of India ?", answer: "Ans: Ram Nath Kovind"},
+    {question: "11. Who is teh current President of India ?", answer: "Ans: Ram Nath Kovind."},
     {question: "12. What is the national flower of India ?", answer: "Ans: Lotus"}
 ]
 
 for(let pos in faqs){
+    card_pos.push(pos);
     var div_elem = document.createElement('div');
     div_elem.setAttribute('class', "div-cards")
     div_elem.setAttribute('id', "div-cards-"+pos);
@@ -24,3 +28,19 @@ for(let pos in faqs){
         document.querySelector('.body-container').appendChild(div_elem);
     }
 }
+
+document.getElementById('search-btn').addEventListener("click", () =>{
+    let item_no =(document.getElementById('search-box').value)-1;
+    if(item_no < card_pos.length){
+        searched_div = document.getElementById('div-cards-'+item_no);
+        searched_div.setAttribute('id', "srch-div")
+        var body_container = document.querySelector('.body-container');
+        while (body_container.hasChildNodes()) {
+            body_container.removeChild(body_container.firstChild);
+         }
+        document.querySelector('.body-container').appendChild(searched_div);
+    }
+    else{
+        alert("Question not found !");
+    }
+})
